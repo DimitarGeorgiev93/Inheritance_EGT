b94762b115e4ffaad4d929c92559d1317cbb8da2
Димитър Георгиев <dggeorgiev93@gmail.com>
2024-03-20 10:35:17 +0200

    minor

--- Venicles/main.cpp
+++ Venicles/main.cpp
@@ -5,6 +5,6 @@
 
 int main()
 {
-    AmphibiousVehicle A("Opel", "Corsa", 4,2000, 20, 42,15,2);
+    AmphibiousVehicle A("Opel", "Corsa", 4,2000, 20, 42,5,2);
     std::cout << A.CanTheVehiclePassTheDistanceUnderAnHour(20,8,2,10);
 }
